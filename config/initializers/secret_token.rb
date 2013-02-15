f76afd7f3377b0049467e3873f5b3c37328b10fb
Joshuwa Jenkins <joshuwa@gmail.com>
2013-02-15 14:17:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etherbasket::Application.config.secret_token = '1b89d24ceca3665edc5671611d01d867a36958f21a8029d97a924c0d3a865fd85f89b875441dadce31b664e8fcda4bf2ee656b9a9a3c755ced2595ec66ebfa66'
